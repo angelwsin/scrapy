3edee18e678fd15164da2ec45f0be1552090f9d3
wuqiang <wuqiang@wdai.com>
2017-12-22 16:11:24 +0800

    验证码识别

--- pydev/com/dev/verify.py
+++ pydev/com/dev/verify.py
@@ -4,6 +4,9 @@ Created on 2017年12月22日
 @author: Administrator
 http://blog.csdn.net/nwpulei/article/details/8457738
 安装模块
+ Pillow(图片处理)tesseract-ocr库支持。
+ https://www.polarxiong.com/archives/python-pytesser-tesseract.html
+ http://blog.csdn.net/huangzhang_123/article/details/64921173
 '''
 
-import 
+
